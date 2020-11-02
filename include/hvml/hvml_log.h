04a0f131d93e1e9641146e2cf2473d1487188678
freemine <freemine@yeah.net>
2020-11-02 20:12:35 +0800

    enable empty-fmt-string in log-macros

--- include/hvml/hvml_log.h
+++ include/hvml/hvml_log.h
@@ -34,16 +34,16 @@ void hvml_log_printf(const char *cfile, int cline, const char *cfunc, FILE *out,
 __attribute__ ((format (printf, 6, 7)));
 
 
-#define D(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'D', fmt, ##__VA_ARGS__)
-#define I(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'I', fmt, ##__VA_ARGS__)
-#define W(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'W', fmt, ##__VA_ARGS__)
-#define E(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'E', fmt, ##__VA_ARGS__)
-#define V(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'V', fmt, ##__VA_ARGS__)
+#define D(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'D', "%s" fmt "", "", ##__VA_ARGS__)
+#define I(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'I', "%s" fmt "", "", ##__VA_ARGS__)
+#define W(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'W', "%s" fmt "", "", ##__VA_ARGS__)
+#define E(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'E', "%s" fmt "", "", ##__VA_ARGS__)
+#define V(fmt, ...) hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'V', "%s" fmt "", "", ##__VA_ARGS__)
 #define A(statement, fmt, ...)                                                  \
 do {                                                                            \
     if (statement) break;                                                       \
     hvml_log_printf(__FILE__, __LINE__, __FUNCTION__, stderr, 'A',              \
-                    "Assert failure:[%s];"fmt, #statement, ##__VA_ARGS__);      \
+                    "Assert failure:[%s];"fmt"", #statement, ##__VA_ARGS__);    \
     abort();                                                                    \
 } while (0)
 
